ba5baeecec7c5a7f2c0b1b1b64b01694d46dda88
Nicholas Macedo <nicholas.francis.macedo@gmail.com>
2016-10-22 13:17:31 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,8 @@ Date: October 5th 2016                         Course Name: CIS 3110
 ****************************************************************************/
 #include "shell.h"
 
-int main(int argc, char const *argv[])
-{
-	system ("clear");
+int main(int argc, char const *argv[]) {
+    system ("clear");
     startShell();  //Calls the main shell function.
-	return 0;
+    return (0);
 }
